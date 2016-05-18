5e292ac096dc949e5fff7b61cdcc4b5e82b58eda
planetpratik <planetpratik@live.in>
2016-05-18 21:40:19 +0530

    Final Commit & Cleaning

--- DOT Logs/Properties/AssemblyInfo.cs
+++ DOT Logs/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DOT Logs")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DOT Lab Records")]
+[assembly: AssemblyDescription("Student Record Management Software")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Department of Technology, Shivaji University, Kolhapur")]
 [assembly: AssemblyProduct("DOT Logs")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Pratik Chougule - B.Tech ( Comp. Science & Tech )  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -53,3 +53,5 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-IN")]
+
